b197678267da4ad3325c06e7b76f5bb5dd21a80c
Slothwi <gabo.leonp@gmail.com>
2025-09-11 18:52:34 -0300

    Add Class producto

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
+from datetime import date
 
 class User(BaseModel):
     id: int
     name: str
     email: str
+
+class Product(BaseModel):
+    ProductoID: int
+    NombreProducto: str
+    FechaCompra: date
+    DuracionGarantia: int
+    Marca: str
+    Modelo: str
+    Tienda: str
+    Notas: str
+    UsuarioID: int
